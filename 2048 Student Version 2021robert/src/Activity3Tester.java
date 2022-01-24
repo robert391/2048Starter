8d519ce2ba81ae4ad79612891059ebd2d69e9355
Almira <Almira@DESKTOP-J3UBQDN>
2022-01-23 19:17:18 -0800

    added a method that moves the elements to the right

--- 2048 Student Version 2021robert/src/Activity3Tester.java
+++ 2048 Student Version 2021robert/src/Activity3Tester.java
@@ -4,17 +4,34 @@ public class Activity3Tester {
 	public static void main(String[] args) {
 		
 		//Declare a Board object
-		
+		Board b = new Board();
 		//create an array with some values you want to 
 		//slideRight. ie [2 0 0 0]
-		
+		int[] sample1 = {4,0,0,0};
+		b.slideRight(sample1);
+		printArray(sample1);
+		int[] sample2 = {2,8,0,2};
+		b.slideRight(sample2);
+		printArray(sample2);
+		int[] sample3 = {2,0,2,0};
+		b.slideRight(sample3);
+		printArray(sample3);
+		int[] sample4 = {0,2,2,0};
+		b.slideRight(sample4);
+		printArray(sample4);
 		//invoke the slideRight method on the array 
 		
 		
 		//print out the array to see if slideRight
 		//worked
 		
-		
+	}
+	public static void printArray(int[] arrays) {
+		String temp = "";
+		for(int i = 0; i < arrays.length; i++) {
+			temp += arrays[i];
+		}
+		System.out.println(temp);
 	}
 
 }
