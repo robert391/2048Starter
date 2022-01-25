c1fef8c7dcaaa15617b1d7dc3e5061de71085e9c
Almira <Almira@DESKTOP-J3UBQDN>
2022-01-24 20:05:25 -0800

    added the populate one

--- 2048 Student Version 2021robert/src/Activity3Tester.java
+++ 2048 Student Version 2021robert/src/Activity3Tester.java
@@ -24,6 +24,7 @@ public class Activity3Tester {
 		
 		//testing the other slide right method
 		Board g = new Board();
+		g.populateOne();
 		System.out.println(g);
 		g.slideRight();
 		System.out.println(g);
